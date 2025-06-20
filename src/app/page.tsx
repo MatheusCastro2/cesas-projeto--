30e5dd00104bb2bff96a5b064a6ece7a8e06f5e1
vitorbanuth <phobinhoo5@gmail.com>
2025-06-20 14:54:48 -0300

    chore: personalized metadata + icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,25 @@ import Cards from "./cards_infos";
 import Informacoes from "./informacoes";
 import Rodape from "./rodape";
 import Video from "./video";
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    
-    <div className='cesas-container'>
-      <Cabecalho />
-      <Informacoes />
-      <Cards />
-      <Video/>
-      <SobrenosEquipe />
-      <Rodape />
-    </div>
+
+    // <><Head>
+    //   <title>Escola CESAS</title>
+    //   <meta name="description" content="Portal Online CESAS" />
+    //   <link rel="icon" href="/favicon.ico" />
+    // </Head>
+
+      <div className='cesas-container'>
+        <Cabecalho />
+        <Informacoes />
+        <Cards />
+        <Video />
+        <SobrenosEquipe />
+        <Rodape />
+      </div>
+    // </>
   );
 }
